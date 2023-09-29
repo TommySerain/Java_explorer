e915d1a4ecef3ed9b7f2cd3ca085da0c8d5e5988
TommySerain <tommyserain@yahoo.fr>
2023-09-29 17:11:31 +0200

    ajout d'un test pour Navigate

--- projet_explorer/src/test/java/co/simplon/alt6/Class/NavigateTest.java
+++ projet_explorer/src/test/java/co/simplon/alt6/Class/NavigateTest.java
@@ -2,23 +2,26 @@ package co.simplon.alt6.Class;
 
 import static org.junit.jupiter.api.Assertions.assertArrayEquals;
 
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 public class NavigateTest {
     Navigate navigation;
 
-    @BeforeEach
-    void setUp() {
+    @Test
+    public void testShowFoldersList() {
         navigation = new Navigate(
                 "C:/Users/Utilisateur/Desktop/CDA/Simplon/Java/dev/projet_explorateur/projet_explorer/src/test/java/co/simplon/alt6/Class/navigateTest");
+        String[] folderList = navigation.ShowFilesList();
+        String[] expected = { "test.txt", "test2.txt", "testNavigate" };
+        assertArrayEquals(expected, folderList);
     }
 
     @Test
-    public void testShowFoldersList() {
-
+    public void testShowFoldersListIfFolderNotExist() {
+        navigation = new Navigate(
+                "C:/blob");
         String[] folderList = navigation.ShowFilesList();
-        String[] expected = { "test.txt", "testNavigate" };
-        assertArrayEquals(expected, folderList);
+        assertArrayEquals(null, folderList);
     }
+
 }
